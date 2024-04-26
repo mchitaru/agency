e0058084622d49d5b72ac5cd623bef65d72c9a73
mchitaru <marius.chitaru@gmail.com>
2024-04-26 12:02:08 +0300

    create agency

---
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = ({
+  params,
+}: {
+  params: {
+    agencyId: string;
+  };
+}) => {
+  return <div>{params.agencyId}</div>;
+};
+
+export default Page;
