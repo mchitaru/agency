880686d599701e5aa9b88031c3fe993dfa616704
mchitaru <marius.chitaru@gmail.com>
2024-05-13 11:36:22 +0300

    subaccountdashboard

--- src/app/(main)/subaccount/page.tsx
+++ src/app/(main)/subaccount/page.tsx
@@ -4,19 +4,18 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
-  searchParams: {
-    state: string;
-    code: string;
-  };
+  searchParams: { state: string; code: string };
 };
 
-const SubaccountPage = async ({ searchParams }: Props) => {
+const SubAccountMainPage = async ({ searchParams }: Props) => {
   const agencyId = await verifyAndAcceptInvitation();
 
-  if (!agencyId) return <Unauthorized />;
+  if (!agencyId) {
+    return <Unauthorized />;
+  }
 
   const user = await getAuthUserDetails();
-  if (!user) return null;
+  if (!user) return;
 
   const getFirstSubaccountWithAccess = user.Permissions.find(
     (permission) => permission.access === true
@@ -26,7 +25,6 @@ const SubaccountPage = async ({ searchParams }: Props) => {
     const statePath = searchParams.state.split("___")[0];
     const stateSubaccountId = searchParams.state.split("___")[1];
     if (!stateSubaccountId) return <Unauthorized />;
-
     return redirect(
       `/subaccount/${stateSubaccountId}/${statePath}?code=${searchParams.code}`
     );
@@ -39,4 +37,4 @@ const SubaccountPage = async ({ searchParams }: Props) => {
   return <Unauthorized />;
 };
 
-export default SubaccountPage;
+export default SubAccountMainPage;
