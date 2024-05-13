cc66ca76af4c826fd39ba9cf9d146d04db80a3cb
mchitaru <marius.chitaru@gmail.com>
2024-05-13 11:24:29 +0300

    webpage editor

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
@@ -8,8 +9,29 @@ const isPublicRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
+  //rewrite for domains
+  const url = req.nextUrl;
+  const searchParams = url.searchParams.toString();
+  let hostname = req.headers;
+
   // if (isProtectedRoute(req)) auth().protect();
   if (!isPublicRoute(req)) auth().protect();
+
+  const pathWithSearchParams = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ""
+  }`;
+
+  //if subdomain exists
+  const customSubDomain = hostname
+    .get("host")
+    ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+    .filter(Boolean)[0];
+
+  if (customSubDomain) {
+    return NextResponse.rewrite(
+      new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+    );
+  }
 });
 
 export const config = {
